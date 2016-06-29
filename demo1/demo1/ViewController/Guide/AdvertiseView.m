89aa033435ffff37c23e5ce949d1c8ec4fb26367
xiaohui2015 <2384618767@qq.com>
2016-06-29 16:27:19 +0800

    clean

--- demo1/demo1/ViewController/Guide/AdvertiseView.m
+++ demo1/demo1/ViewController/Guide/AdvertiseView.m
@@ -64,28 +64,28 @@ static int const advertiseDuration = 3;
 //    [window addSubview:self];
 }
 
-- (void)startCountDownWithGCD
-{
-    __block int timeout = advertiseDuration + 1;
-    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
-    dispatch_source_set_timer(timer, dispatch_walltime(NULL, 0), 1.0*NSEC_PER_SEC, 0);
-    dispatch_source_set_event_handler(timer, ^{
-        if (timeout <= 0) {
-            dispatch_source_cancel(timer);
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self dismiss];
-            });
-        }
-        else {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [countBtn setTitle:[NSString stringWithFormat:@"跳过%d",timeout] forState:UIControlStateNormal];
-            });
-            timeout --;
-        }
-    });
-    dispatch_resume(timer);
-}
+//- (void)startCountDownWithGCD
+//{
+//    __block int timeout = advertiseDuration + 1;
+//    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+//    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+//    dispatch_source_set_timer(timer, dispatch_walltime(NULL, 0), 1.0*NSEC_PER_SEC, 0);
+//    dispatch_source_set_event_handler(timer, ^{
+//        if (timeout <= 0) {
+//            dispatch_source_cancel(timer);
+//            dispatch_async(dispatch_get_main_queue(), ^{
+//                [self dismiss];
+//            });
+//        }
+//        else {
+//            dispatch_async(dispatch_get_main_queue(), ^{
+//                [countBtn setTitle:[NSString stringWithFormat:@"跳过%d",timeout] forState:UIControlStateNormal];
+//            });
+//            timeout --;
+//        }
+//    });
+//    dispatch_resume(timer);
+//}
 
 - (void)startTimer
 {
