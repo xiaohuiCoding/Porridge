d4de6f3c6fb6a10726cdbcccd9fa33bbb872cc2c
fengxiaohui <18367107969@163.com>
2016-08-04 16:37:31 +0800

    增加播放功能

--- demo1/demo1/Tool/AVAudioManager.m
+++ demo1/demo1/Tool/AVAudioManager.m
@@ -36,8 +36,18 @@
     }
     item = [[AVPlayerItem alloc] initWithURL:[NSURL URLWithString:urlString]];
     [self.player replaceCurrentItemWithPlayerItem:item];
+    //self.player = [[AVPlayer alloc] initWithPlayerItem:item];
     
     [item addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(SingleCycle) name:AVPlayerItemDidPlayToEndTimeNotification object:item];
+}
+
+- (void)SingleCycle
+{
+    [self.player seekToTime:CMTimeMake(0, 1)];
+    [self play];
+    NSLog(@"single cycle ing!");
 }
 
 - (void)play
@@ -56,6 +66,7 @@
     }
     [self.player pause];
     isPlaying = NO;
+    NSLog(@"the song is paused!");
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
@@ -64,18 +75,21 @@
     
     AVPlayerItemStatus status = [change[@"new"] integerValue];
     
+    NSLog(@"current status = %ld",status);
+    
     switch (status) {
             
-        case 0:
-            NSLog(@"unknown resource!");
+        case AVPlayerItemStatusUnknown:
+            NSLog(@"unknown status!");
             break;
         
-        case 1:
+        case AVPlayerItemStatusReadyToPlay:
+            NSLog(@"ready to play!");
             isReady = YES;
             [self play];
             break;
             
-        case 2:
+        case AVPlayerItemStatusFailed:
             NSLog(@"play failed!");
             break;
             
--- demo1/demo1/ViewController/Mine/MyMusicViewController.m
+++ demo1/demo1/ViewController/Mine/MyMusicViewController.m
@@ -12,8 +12,8 @@
 
 @interface MyMusicViewController ()
 {
-    NSInteger currentIndex;
     NSArray   *urlStringArray;
+    NSInteger currentIndex;
 }
 @end
 
@@ -22,60 +22,75 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    currentIndex = 0;
-    
     urlStringArray = @[@"http://m1.music.126.net/gpi8Adr_-pfCuP7ZXk_F2w==/2926899953898363.mp3",
                        @"http://data.5sing.kgimg.com/G061/M0A/03/13/HZQEAFb493iAOeg5AHMiAfzZU0E739.mp3",
                        @"http://m1.music.126.net/gpi8Adr_-pfCuP7ZXk_F2w==/2926899953898363.mp3",
                        @"http://data.5sing.kgimg.com/G061/M0A/03/13/HZQEAFb493iAOeg5AHMiAfzZU0E739.mp3"];
     
-    UIButton *btn1 = [XHObject newButton:CGRectMake(100, 100, 60, 40) title:@"开始" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn1 = [XHObject newButton:CGRectMake(100, 100, 60, 40) title:@"第一首" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn1.backgroundColor = [UIColor redColor];
-    [btn1 addTarget:self action:@selector(startPlaySong) forControlEvents:UIControlEventTouchUpInside];
+    [btn1 addTarget:self action:@selector(playTheFirst) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn1];
     
-    UIButton *btn2 = [XHObject newButton:CGRectMake(100, 200, 60, 40) title:@"切歌" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn2 = [XHObject newButton:CGRectMake(200, 100, 60, 40) title:@"暂停" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn2.backgroundColor = [UIColor redColor];
-    [btn2 addTarget:self action:@selector(playNextSong:) forControlEvents:UIControlEventTouchUpInside];
+    [btn2 addTarget:self action:@selector(playPause) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn2];
+
+    UIButton *btn3 = [XHObject newButton:CGRectMake(100, 200, 60, 40) title:@"上一首" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    btn3.backgroundColor = [UIColor redColor];
+    [btn3 addTarget:self action:@selector(playTheLast:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn3];
+    
+    UIButton *btn4 = [XHObject newButton:CGRectMake(200, 200, 60, 40) title:@"下一首" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    btn4.backgroundColor = [UIColor redColor];
+    [btn4 addTarget:self action:@selector(playTheNext:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn4];
+    
+    currentIndex = 0;
 }
 
-- (void)startPlaySong
+- (void)playTheFirst
 {
     AVAudioManager *manager = [AVAudioManager sharedManager];
-    [manager playWithURLString:urlStringArray[currentIndex]];
-    [manager play];
+    [manager playWithURLString:urlStringArray[0]];
     NSLog(@"currentIndex = %ld", currentIndex);
 }
 
-- (void)playNextSong:(NSString *)url
+- (void)playPause
 {
-    currentIndex ++;
+    AVAudioManager *manager = [AVAudioManager sharedManager];
+    [manager pause];
+}
+
+- (void)playTheLast:(NSString *)url
+{
+    if (currentIndex == 0) {
+        currentIndex = urlStringArray.count;
+    }
     
+    currentIndex --;
     url = urlStringArray[currentIndex];
     [[AVAudioManager sharedManager] playWithURLString:url];
-    [[AVAudioManager sharedManager] play];
-    
     NSLog(@"currentIndex = %ld", currentIndex);
-    
+}
+
+- (void)playTheNext:(NSString *)url
+{
     if (currentIndex == urlStringArray.count - 1) {
         currentIndex = -1;
     }
+    
+    currentIndex ++;
+    url = urlStringArray[currentIndex];
+    [[AVAudioManager sharedManager] playWithURLString:url];
+    
+    NSLog(@"currentIndex = %ld", currentIndex);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
