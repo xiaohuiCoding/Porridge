6d652b1ee629ed4da371b8175fd61b1107cd4d9f
fengxiaohui <18367107969@163.com>
2017-03-20 15:35:11 +0800

    add another getCurrentVC method

--- demo1/demo1/Tool/XHObject.m
+++ demo1/demo1/Tool/XHObject.m
@@ -26,6 +26,30 @@
 }
 
 //获取当前屏幕显示的viewController
++ (UIViewController *)getCurrentVCCC
+{
+    UIViewController *result = nil;
+    UIWindow * window = [[UIApplication sharedApplication] keyWindow];
+    if (window.windowLevel != UIWindowLevelNormal)
+    {
+        NSArray *windows = [[UIApplication sharedApplication] windows];
+        for(UIWindow * tmpWin in windows) {
+            if (tmpWin.windowLevel == UIWindowLevelNormal) {
+                window = tmpWin;
+                break;
+            }
+        }
+    }
+    UIView *frontView = [[window subviews] objectAtIndex:0];
+    id nextResponder = [frontView nextResponder];
+    if ([nextResponder isKindOfClass:[UIViewController class]])
+        result = nextResponder;
+    else
+        result = window.rootViewController;
+    return result;
+}
+
+//获取当前屏幕显示的viewController
 + (UIViewController *)getCurrentVC
 {
     UIViewController * currVC = nil;
