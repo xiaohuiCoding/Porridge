fcda7c4065d8d1c90175fdc733ca46c3f1fc13b6
xiaohuiCoding <2384618767@qq.com>
2017-06-16 18:10:33 +0800

    clean files

--- demo1/demo1/ViewController/Mine/MyMovieViewController.m
+++ demo1/demo1/ViewController/Mine/MyMovieViewController.m
@@ -25,22 +25,22 @@
     urlStringArray = @[@"http://static.tripbe.com/videofiles/20121214/9533522808.f4v.mp4",
                        @"http://v.jxvdy.com/sendfile/w5bgP3A8JgiQQo5l0hvoNGE2H16WbN09X-ONHPq3P3C1BISgf7C-qVs6_c8oaw3zKScO78I--b0BGFBRxlpw13sf2e54QA"];
     
-    UIButton *btn1 = [XHObject newButton:CGRectMake(100, 100, 60, 40) title:@"第一部" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn1 = [GlobalObject newButton:CGRectMake(100, 100, 60, 40) title:@"第一部" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn1.backgroundColor = [UIColor redColor];
     [btn1 addTarget:self action:@selector(playTheFirst:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn1];
     
-    UIButton *btn2 = [XHObject newButton:CGRectMake(200, 100, 60, 40) title:@"暂停" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn2 = [GlobalObject newButton:CGRectMake(200, 100, 60, 40) title:@"暂停" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn2.backgroundColor = [UIColor redColor];
     [btn2 addTarget:self action:@selector(playPause) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn2];
     
-    UIButton *btn3 = [XHObject newButton:CGRectMake(100, 200, 60, 40) title:@"上一部" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn3 = [GlobalObject newButton:CGRectMake(100, 200, 60, 40) title:@"上一部" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn3.backgroundColor = [UIColor redColor];
     [btn3 addTarget:self action:@selector(playTheLast:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn3];
     
-    UIButton *btn4 = [XHObject newButton:CGRectMake(200, 200, 60, 40) title:@"下一部" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn4 = [GlobalObject newButton:CGRectMake(200, 200, 60, 40) title:@"下一部" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn4.backgroundColor = [UIColor redColor];
     [btn4 addTarget:self action:@selector(playTheNext:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn4];
--- demo1/demo1/ViewController/Mine/MyMusicViewController.m
+++ demo1/demo1/ViewController/Mine/MyMusicViewController.m
@@ -27,22 +27,22 @@
                        @"http://m1.music.126.net/gpi8Adr_-pfCuP7ZXk_F2w==/2926899953898363.mp3",
                        @"http://data.5sing.kgimg.com/G061/M0A/03/13/HZQEAFb493iAOeg5AHMiAfzZU0E739.mp3"];
     
-    UIButton *btn1 = [XHObject newButton:CGRectMake(100, 100, 60, 40) title:@"第一首" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn1 = [GlobalObject newButton:CGRectMake(100, 100, 60, 40) title:@"第一首" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn1.backgroundColor = [UIColor redColor];
     [btn1 addTarget:self action:@selector(playTheFirst:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn1];
     
-    UIButton *btn2 = [XHObject newButton:CGRectMake(200, 100, 60, 40) title:@"暂停" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn2 = [GlobalObject newButton:CGRectMake(200, 100, 60, 40) title:@"暂停" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn2.backgroundColor = [UIColor redColor];
     [btn2 addTarget:self action:@selector(playPause) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn2];
 
-    UIButton *btn3 = [XHObject newButton:CGRectMake(100, 200, 60, 40) title:@"上一首" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn3 = [GlobalObject newButton:CGRectMake(100, 200, 60, 40) title:@"上一首" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn3.backgroundColor = [UIColor redColor];
     [btn3 addTarget:self action:@selector(playTheLast:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn3];
     
-    UIButton *btn4 = [XHObject newButton:CGRectMake(200, 200, 60, 40) title:@"下一首" titleColor:nil normalBkg:nil highlightedBkg:nil];
+    UIButton *btn4 = [GlobalObject newButton:CGRectMake(200, 200, 60, 40) title:@"下一首" titleColor:nil normalBkg:nil highlightedBkg:nil];
     btn4.backgroundColor = [UIColor redColor];
     [btn4 addTarget:self action:@selector(playTheNext:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn4];
